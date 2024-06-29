836a420b8ce1933aad9dbb026fa2b59b58360920
williford-abunas <abunaswilliford@gmail.com>
2024-06-29 22:18:19 +1200

    baseURL to /api/notes

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 export const getall = async () => {
   const res = await axios.get(baseUrl)
